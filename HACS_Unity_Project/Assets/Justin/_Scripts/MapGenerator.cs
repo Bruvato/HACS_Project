cc5d4748338661ca5852ca205a0087638de5c70c
jjustinjc <63550112+jjustinjc@users.noreply.github.com>
2023-03-13 09:24:04 -0400

    big stuff
    made map gen workj

--- HACS_Unity_Project/Assets/Justin/_Scripts/MapGenerator.cs
+++ HACS_Unity_Project/Assets/Justin/_Scripts/MapGenerator.cs
@@ -17,12 +17,12 @@ public class MapGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        buildingLayout = new bool[mLength,mLength];
+        buildingLayout = new bool[mLength, mLength];
         for (int i = 0; i < buildingLayout.GetLength(0); i++)
         {
-            for(int j = 0; j< buildingLayout.GetLength(1); j++)
+            for (int j = 0; j < buildingLayout.GetLength(1); j++)
             {
-                buildingLayout[i,j]=false;
+                buildingLayout[i, j] = false;
             }
         }
         numBuildings = mLength;
@@ -39,73 +39,99 @@ public class MapGenerator : MonoBehaviour
 
     }
     void GenerateTiles(int r, int c)
+    {
+        if (generatedBuildings < numBuildings)
         {
-            if (generatedBuildings < numBuildings)
+            int[] possiblePositions = new int[9];
+            for (int i = 0; i < possiblePositions.Length; i++)
             {
-                int[] possiblePositions = new int[9];
+                possiblePositions[i] = i;
+            }
+            // if (r - 1 == -1)
+            // {
+            //     possiblePositions[0] = -1;
+            //     possiblePositions[1] = -1;
+            //     possiblePositions[2] = -1;
+            // }
 
-                if (r - 1 == -1)
+            // if (c - 1 == -1)
+            // {
+            //     possiblePositions[0] = -1;
+            //     possiblePositions[3] = -1;
+            //     possiblePositions[6] = -1;
+            // }
+
+
+
+            int count = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
                 {
-                    possiblePositions[0] = -1;
-                    possiblePositions[1] = -1;
-                    possiblePositions[2] = -1;
-                }
 
-                if (c - 1 == -1)
-                {
-                    possiblePositions[0] = -1;
-                    possiblePositions[3] = -1;
-                    possiblePositions[6] = -1;
-                }
-
-
-
-                int count = 0;
-                for (int i = -1; i < 2; i++)
-                {
-                    for (int j = -1; j < 2; j++)
+                    if (possiblePositions[count] != -1)
                     {
-                        if (possiblePositions[count] != -1)
+                        if (isIn2dBoolArray(r + i, c + j, buildingLayout))
                         {
-                            if(buildingLayout[r + i,c + j]==true){
-                            possiblePositions[count] = -1;
+                            if (buildingLayout[r + i, c + j] == true)
+                            {
+
+                                possiblePositions[count] = -1;
                             }
                         }
-                        count++;
                     }
+                    count++;
                 }
+            }
 
 
 
-                int maxPossible = 0;
-                foreach (int n in possiblePositions)
+            int maxPossible = 0;
+            foreach (int n in possiblePositions)
+            {
+                if (n != -1)
                 {
-                    if (n != -1)
-                    {
-                        maxPossible++;
-                    }
+                    maxPossible++;
                 }
+            }
 
-                int generated = Random.Range(0, maxPossible);
-                int scroller = 0;
-                for (int i = -1; i < 2; i++)
+            int generated = Random.Range(0, maxPossible);
+            int scroller = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
                 {
-                    for (int j = -1; j < 2; j++)
+                    if (scroller == generated && possiblePositions[scroller] != -1)
                     {
-                        if (scroller == generated&&possiblePositions[scroller]!=-1)
+                        if (isIn2dBoolArray(r + i, c + j, buildingLayout))
                         {
-                            buildingLayout[r + i,c + j] = true;
+                            buildingLayout[r + i, c + j] = true;
                             generatedBuildings++;
                             lastGeneratedr = r + i;
                             lastGeneratedc = c + i;
                         }
-                        scroller++;
-                        if(possiblePositions[scroller]==-1){
-                            generated++;
-                        }
                     }
-                }
+                    else if (possiblePositions[scroller] == -1)
+                    {
+                        generated++;
+                    }
+                    scroller++;
 
+                }
             }
+
         }
+    }
+    private bool isIn2dBoolArray(int row, int col, bool[,] arr)
+    {
+        return row < arr.GetLength(0) && row > -1 && col < arr.GetLength(1) && col > -1;
+
+    }
+    private bool isIn2dIntArray(int row, int col, int[,] arr)
+    {
+        return row < arr.GetLength(0) && row > -1 && col < arr.GetLength(1) && col > -1;
+
+    }
 }
+
+
